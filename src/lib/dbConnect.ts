32b6e9a903ef16fa4f90a3e47884d8101965a6ca
Manish Prajapati <manishprajapatim416@gmail.com>
2024-07-05 00:38:49 +0530

    Updated dbConnect file

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,5 +1,50 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions, Connection } from "mongoose";
 
-export default async function DBConnect() {
-  return mongoose.connect(process.env.MONGODB_URI!);
+const MONGODB_URI = process.env.MONGODB_URI;
+
+interface MongooseCache {
+  conn: Connection | null;
+  promise: Promise<Connection> | null;
 }
+
+declare global {
+  // Allow global `mongoose` cache in development
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function DBConnect(): Promise<Connection> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const options: ConnectOptions = {};
+
+    if (!MONGODB_URI) {
+      throw new Error(
+        "Please define the MONGODB_URI environment variable inside .env.local"
+      );
+    }
+
+    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
+      return mongoose.connection;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default DBConnect;
