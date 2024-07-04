b9cdaf238b1274623bee2477100e1f4595b6d1d0
Manish Prajapati <manishprajapatim416@gmail.com>
2024-07-04 17:54:07 +0530

    Disabling button when clicked

--- src/components/buttons/LoginWithGoogle.tsx
+++ src/components/buttons/LoginWithGoogle.tsx
@@ -2,20 +2,28 @@
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signIn } from "next-auth/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface LoginWithGoogleProps {}
 
 const LoginWithGoogle: FC<LoginWithGoogleProps> = ({}) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSignIn = async () => {
+    setLoading(true);
+    return await signIn("google", {
+      redirect: true,
+      callbackUrl: "/account",
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
+
   return (
     <button
-      onClick={() =>
-        signIn("google", {
-          redirect: true,
-          callbackUrl: "/account",
-        })
-      }
-      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center"
+      onClick={handleSignIn}
+      disabled={loading}
+      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center disabled:bg-gray-200 disabled:blur-[1px]"
     >
       <FontAwesomeIcon icon={faGoogle} className="h-6" />
       <span>Sign in with Google</span>
